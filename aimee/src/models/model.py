bfadc612f1a37185050f04ff69ce66a5aca77fdd
Ilir Gusija <78830837+ilirgusija@users.noreply.github.com>
2024-03-30 17:35:32 -0400

    new hyperprior

---
+++ aimee/src/models/model.py
@@ -0,0 +1,213 @@
+import torch
+from torch import nn
+import numpy as np
+from torch.nn import functional as F
+import zlib
+import bz2
+import lzma
+import pickle
+
+def uniform_quantizer(x):
+    # Define the range of input values
+    quantized_values = torch.round(x)
+    return quantized_values
+
+class Quantizer_Gaussian(nn.Module):
+    def __init__(self, N_input=1, N_bottleneck=7, N_output=1):
+        super(Quantizer_Gaussian, self).__init__()
+        self.delta = 1.0/2.0
+        N2 = 3
+        N3 = 5
+        # N2 = int(N_input/2)
+        # N3= int(N2/2)
+        self.fc1_e = nn.Linear(N_input, N2)
+        self.fc2_e = nn.Linear(N2, N3)
+        self.fc3_e = nn.Linear(N3, N_bottleneck)
+
+        self.fc1_d = nn.Linear(N_bottleneck, N3)
+        self.fc2_d = nn.Linear(N3, N2)
+        self.fc3_d = nn.Linear(N2, N_output)
+
+    def transform(self, X):
+        X = self.fc1_e(X)
+        X = F.relu(X)
+        X = self.fc2_e(X)
+        X = F.relu(X)
+        X = self.fc3_e(X)
+        X = F.relu(X)
+        return X
+
+    def inverse_transform(self, X):
+        X = self.fc1_d(X)
+        X = F.relu(X)
+        X = self.fc2_d(X)
+        X = F.relu(X)
+        X = self.fc3_d(X)
+        return X
+
+    def forward(self, X):
+        # Encode then decode the input
+        features = self.transform(X)
+        if self.training:
+            noise = torch.rand_like(features) - 0.5
+            quantized = features + noise
+            output = self.inverse_transform(quantized)
+        else:
+            quantized = uniform_quantizer(features)
+            output = self.inverse_transform(quantized)
+        return output, quantized
+
+class MNIST_Coder(nn.Module):
+    def __init__(self, N_input=784, N_bottleneck=8, N_output=784, compression_method='zlib'):
+        super(MNIST_Coder, self).__init__()
+        N2 = 392
+        N3 = int(N2/2)
+        
+        self.fc1_e = nn.Linear(N_input, N2)
+        self.fc2_e = nn.Linear(N2, N_bottleneck)
+        # self.fc3_e = nn.Linear(N3, N_bottleneck)
+        
+        # self.fc1_d = nn.Linear(N_bottleneck, N3)
+        self.fc2_d = nn.Linear(N_bottleneck, N2)
+        self.fc3_d = nn.Linear(N2, N_output)
+        
+        self.compression_method = compression_method
+
+    def transform(self, X):
+        X = self.fc1_e(X)
+        X = F.relu(X)
+        X = self.fc2_e(X)
+        # X = F.relu(X)
+        # X = self.fc3_e(X)
+        X = F.relu(X)
+        return X
+
+    def inverse_transform(self, X):
+        # X = self.fc1_d(X)
+        # X = F.relu(X)
+        X = self.fc2_d(X)
+        X = F.relu(X)
+        X = self.fc3_d(X)
+        X = F.sigmoid(X)
+        return X
+    
+    def encode(self, data):
+        # Serialize the quantized data from Tensor to a format that our encoder can handle
+        data = pickle.dumps(data)
+        
+        if self.compression_method == 'zlib':
+            return zlib.compress(data)
+        elif self.compression_method == 'bz2':
+            return bz2.compress(data)
+        elif self.compression_method == 'lzma':
+            return lzma.compress(data)
+        else:
+            raise ValueError("Unsupported compression method")
+
+    def decode(self, data):
+        if self.compression_method == 'zlib':
+            data = zlib.decompress(data)
+        elif self.compression_method == 'bz2':
+            data = bz2.decompress(data)
+        elif self.compression_method == 'lzma':
+            data = lzma.decompress(data)
+        else:
+            raise ValueError("Unsupported decompression method")
+        
+        # Deserialize the output of our quantizer
+        data = pickle.loads(data)
+        return data
+    
+    def compress(self, X):
+        # Run the neural network
+        features = self.transform(X)
+        
+        # Quantize our features
+        quantized = uniform_quantizer(features)
+        
+        # Losslessly code using the method defined at initialization
+        encoded = self.encode(quantized)
+        return encoded
+
+    def decompress(self, X):
+        # Run the decoder on our compressed file
+        decoded = self.decode(X)
+        
+        # Run neural network
+        output = self.inverse_transform(decoded)
+        
+        return output
+
+    def forward(self, X):
+        features = self.transform(X)
+        if self.training:
+            noise = torch.rand_like(features) - 0.5
+            quantized = features + noise
+            output = self.inverse_transform(quantized)
+        else:
+            quantized = uniform_quantizer(features)
+            output = self.inverse_transform(quantized)
+        return output, quantized
+
+class MNIST_FCNN(nn.Module):
+    def __init__(self):
+        super(MNIST_FCNN, self).__init__()
+        # Encoder layers
+        self.transform = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=2, padding=1),  # Output: (16, 14, 14)
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 3, stride=2, padding=1), # Output: (32, 7, 7)
+            nn.ReLU(), 
+            nn.Conv2d(32, 64, 7)                      # Output: (64, 1, 1)
+        )
+        # Decoder layers
+        self.inverse_transform = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, 7),            # Output: (32, 7, 7)
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1), # Output: (16, 14, 14)
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1),  # Output: (1, 28, 28)
+            nn.Sigmoid()  # Using sigmoid for output to get values between 0 and 1
+        )
+
+    def forward(self, X):
+        features = self.transform(X)
+        if self.training:
+            noise = torch.rand_like(features) - 0.5
+            quantized = features + noise
+            output = self.inverse_transform(quantized)
+        else:
+            quantized = uniform_quantizer(features)
+            output = self.inverse_transform(quantized)
+        return output, quantized
+
+class MNIST_VAE(nn.Module):
+    def __init__(self):
+        super(MNIST_VAE, self).__init__()
+        
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(32 * 7 * 7, 128),
+            nn.ReLU(),
+        )
+
+    def analysis(self, X):
+        return 0
+
+    def synthesis(self, X):
+        return 0
+
+    
+
+
+    def forward(self, x):
+        x = self.encoder(x)
+        mu = self.fc_mu(x)
+        logvar = self.fc_logvar(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decoder(z), mu, logvar
---
+++ aimee/src/models/model_hyperprior.py
@@ -0,0 +1,149 @@
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from sklearn.datasets import load_digits
+import compressai
+from compressai.entropy_models import EntropyBottleneck, GaussianConditional
+from compressai.layers import GDN
+from model import Quantizer_Gaussian
+import torch.nn.functional as F
+from torchxrayvision.datasets import NIH_Dataset
+
+
+import matplotlib
+matplotlib.use('GTK3Agg')
+from matplotlib import pyplot as plt
+
+import numpy as np
+
+def conv(in_channels, out_channels, kernel_size=5, stride=2):
+    return nn.Conv2d(
+        in_channels,
+        out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=kernel_size // 2,
+    )
+
+
+def deconv(in_channels, out_channels, kernel_size=5, stride=2):
+    return nn.ConvTranspose2d(
+        in_channels,
+        out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+        output_padding=stride - 1,
+        padding=kernel_size // 2,
+    )
+
+class ScaleHyper(Quantizer_Gaussian):
+    def __init__(self, N, M, **kwargs):
+        super().__init__(**kwargs)
+
+        self.entropy_bottleneck = EntropyBottleneck(N)
+
+        self.g_a = nn.Sequential(
+            conv(1, N),
+            GDN(N),
+            conv(N, N),
+            GDN(N),
+            conv(N, N),
+            GDN(N),
+            conv(N, M),
+        )
+
+        self.g_s = nn.Sequential(
+            deconv(M, N),
+            GDN(N, inverse=True),
+            deconv(N, N),
+            GDN(N, inverse=True),
+            deconv(N, N),
+            GDN(N, inverse=True),
+            deconv(N, 1),
+        )
+
+        self.h_a = nn.Sequential(
+            conv(M, N, stride=1, kernel_size=3),
+            nn.ReLU(inplace=True),
+            conv(N, N),
+            nn.ReLU(inplace=True),
+            conv(N, N),
+        )
+
+        self.h_s = nn.Sequential(
+            deconv(N, N),
+            nn.ReLU(inplace=True),
+            deconv(N, N),
+            nn.ReLU(inplace=True),
+            conv(N, M, stride=1, kernel_size=3),
+            nn.ReLU(inplace=True),
+        )
+
+        self.gaussian_conditional = GaussianConditional(None)
+        self.N = int(N)
+        self.M = int(M)
+
+    @property
+    def downsampling_factor(self) -> int:
+        return 2 ** (4 + 2)
+
+    def forward(self, x):        
+        y = self.g_a(x)
+        z = self.h_a(torch.abs(y))
+        z_hat, z_likelihoods = self.entropy_bottleneck(z)
+        scales_hat = self.h_s(z_hat)
+        y_hat, y_likelihoods = self.gaussian_conditional(y, scales_hat)
+        x_hat = self.g_s(y_hat)
+
+        return {
+            "x_hat": x_hat,
+            "likelihoods": {"y": y_likelihoods, "z": z_likelihoods},
+        }
+
+    @classmethod
+    def from_state_dict(cls, state_dict):
+        """Return a new model instance from `state_dict`."""
+        N = state_dict["g_a.0.weight"].size(0)
+        M = state_dict["g_a.6.weight"].size(0)
+        net = cls(N, M)
+        net.load_state_dict(state_dict)
+        return net
+
+    def compress(self, x):
+        y = self.g_a(x)
+        z = self.h_a(torch.abs(y))
+
+        z_strings = self.entropy_bottleneck.compress(z)
+        z_hat = self.entropy_bottleneck.decompress(z_strings, z.size()[-2:])
+
+        scales_hat = self.h_s(z_hat)
+        indexes = self.gaussian_conditional.build_indexes(scales_hat)
+        y_strings = self.gaussian_conditional.compress(y, indexes)
+        return {"strings": [y_strings, z_strings], "shape": z.size()[-2:]}
+
+    def decompress(self, strings, shape):
+        assert isinstance(strings, list) and len(strings) == 2
+        z_hat = self.entropy_bottleneck.decompress(strings[1], shape)
+        scales_hat = self.h_s(z_hat)
+        indexes = self.gaussian_conditional.build_indexes(scales_hat)
+        y_hat = self.gaussian_conditional.decompress(strings[0], indexes, z_hat.dtype)
+        x_hat = self.g_s(y_hat).clamp_(0, 1)
+        return {"x_hat": x_hat}
+
+
+def main():
+    b = 64
+    dataset_path = "/data/user3/data-resized/NIH/images-224"
+    train_dataset = NIH_Dataset(dataset_path)
+    # test_dataset = NIH_Dataset(dataset_path)
+    
+    train_loader = DataLoader(train_dataset, batch_size=b, shuffle=True)
+    # img = dataset[0]["img"]
+    # plt.imshow(np.squeeze(img))
+    # plt.show()
+    return 0
+
+if __name__ == "__main__":
+    main()
